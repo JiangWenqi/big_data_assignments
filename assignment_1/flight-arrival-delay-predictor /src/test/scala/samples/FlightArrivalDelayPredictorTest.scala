8aa0f932964c89a6cb6631d170ee33e46c642a63
JiangWenqi <jiangwenqi1995@gmail.com>
2021-10-25 23:23:37 +0200

    add readme and test

---
+++ assignment_1/flight-arrival-delay-predictor /src/test/scala/samples/FlightArrivalDelayPredictorTest.scala
@@ -0,0 +1,17 @@
+package samples
+
+import org.junit._
+import Assert._
+
+@Test
+class FlightArrivalDelayPredictorTest {
+
+    @Test
+    def testOK(): Unit = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
